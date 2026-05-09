ad72879c96b62946786652881769ced1a20168df
Stefan <stefan@example.com>
2026-05-09 23:13:26 +0200

    Versione 5 pagine separate: ripristinata LoginView e pulizia finale

--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import LoginView from '../views/LoginView.vue'
 import HomeView from '../views/HomeView.vue'
 import GameView from '../views/GameView.vue'
 import ProfileView from '../views/ProfileView.vue'
 import LeaderboardView from '../views/LeaderboardView.vue'
-import AboutView from '../views/AboutView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    { path: '/login', name: 'login', component: LoginView },
     { path: '/', name: 'home', component: HomeView },
     { path: '/game', name: 'game', component: GameView },
     { path: '/profile', name: 'profile', component: ProfileView },
-    { path: '/leaderboard', name: 'leaderboard', component: LeaderboardView },
-    { path: '/about', name: 'about', component: AboutView }
+    { path: '/leaderboard', name: 'leaderboard', component: LeaderboardView }
   ]
 })
 
